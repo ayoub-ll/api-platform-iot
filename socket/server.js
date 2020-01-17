51daa31588f1256dea7c1a61b2732c9f970776c3
lolalfg <56671149+lolalfg@users.noreply.github.com>
2020-01-17 16:24:03 +0100

    connexion nodeJs a la base

--- socket/server.js
+++ socket/server.js
@@ -1,6 +1,13 @@
 const io = require('socket.io')();
 var SerialPort = require('serialport');
 var xbee_api = require('xbee-api');
+let request = require('request');
+
+
+
+
+let arrayPassword = RecoverPassword();
+
 var C = xbee_api.constants;
 
 let password = "BJJBBBJJJJ";
@@ -17,28 +24,24 @@ var code_user = 3434;
 
 var enableGreenLight = {
   type: 0x17, // xbee_api.constants.FRAME_TYPE.AT_COMMAND
-  //id: 0x52, // optional, nextFrameId() is called per default
   command: "D2",
   commandParameter: [0x05],
 };
 
 var disableGreenLight = {
   type: 0x17, // xbee_api.constants.FRAME_TYPE.AT_COMMAND
-  //id: 0x52, // optional, nextFrameId() is called per default
   command: "D2",
   commandParameter: [0x00],
 };
 
 var disableRedLight = {
   type: 0x17, // xbee_api.constants.FRAME_TYPE.AT_COMMAND
-  //id: 0x52, // optional, nextFrameId() is called per default
   command: "D3",
   commandParameter: [0x00],
 };
 
 var enableRedLight = {
   type: 0x17, // xbee_api.constants.FRAME_TYPE.AT_COMMAND
-  //id: 0x52, // optional, nextFrameId() is called per default
   command: "D3",
   commandParameter: [0x05],
 };
@@ -48,13 +51,15 @@ var xbeeAPI = new xbee_api.XBeeAPI({
 });
 
 //let serialport = new SerialPort("/dev/tty.SLAB_USBtoUART", {
-let serialport = new SerialPort("COM3",{
+//let serialport = new SerialPort("COM3",{
+let serialport = new SerialPort("COM4",{
   baudRate: 9600,
 }, function (err) {
   if (err) {
     return console.log('Error: ', err.message)
   }
 });
+
 //xbeeAPI.builder.write(enableRedLight);
 //xbeeAPI.builder.write(disableGreenLight);
 
@@ -83,9 +88,9 @@ serialport.on("open", function () {
 });
 
 // All frames parsed by the XBee will be emitted here
-
 xbeeAPI.parser.on("data", function (frame) {
   //on new device is joined, register it
+
 
   if(frame.digitalSamples !== undefined)
   {
@@ -99,41 +104,26 @@ xbeeAPI.parser.on("data", function (frame) {
     }
 
     // Si la combinaison entré est la bonne, on allume la led verte pendant 10sec
-      if(!truePass && combinationEnteredString.match(password)){
-        truePass = true;
-        xbeeAPI.builder.write(enableGreenLight);
-        xbeeAPI.builder.write(disableRedLight);
+    if(!truePass && combinationEnteredString.match(password)){
+      truePass = true;
+      xbeeAPI.builder.write(enableGreenLight);
+      xbeeAPI.builder.write(disableRedLight);
 
-        console.log(combinationEnteredString + " : Combinaison correct !");
+      console.log(combinationEnteredString + " : Combinaison correct !");
 
-        setTimeout(function(){
-          xbeeAPI.builder.write(disableGreenLight);
-          xbeeAPI.builder.write(enableRedLight);
-          combinationEnteredString = "";
-          truePass = false;
-        },3000);
-      }
-      else{
-        console.log(combinationEnteredString);
-      }
+      setTimeout(function(){
+        xbeeAPI.builder.write(disableGreenLight);
+        xbeeAPI.builder.write(enableRedLight);
+        combinationEnteredString = "";
+        truePass = false;
+      },3000);
+    }
+    else{
+      console.log(combinationEnteredString);
+    }
 
   }
 
-    //jsonFrameDigi = JSON.stringify(frame.digitalSamples['DIO0']);
-
-
-  //combinationEnteredString += jsonFrameDigi;
-
-  /*
-  if(jsonFrameDigi == JSON.stringify(blueButtonEnabled))
-    console.log("bleu bleu bleu");
-
-  if(jsonFrameDigi == JSON.stringify(whiteButtonEnabled))
-    xbeeAPI.builder.write()
-  */
-
-  //on packet received, dispatch event
-  //let dataReceived = String.fromCharCode.apply(null, frame.data);
   if (C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET === frame.type) {
     console.log("C.FRAME_TYPE.ZIGBEE_RECEIVE_PACKET");
     let dataReceived = String.fromCharCode.apply(null, frame.data);
@@ -146,24 +136,18 @@ xbeeAPI.parser.on("data", function (frame) {
   }
 
   if (C.FRAME_TYPE.NODE_IDENTIFICATION === frame.type) {
-    //console.log(frame);
-    // let dataReceived = String.fromCharCode.apply(null, frame.nodeIdentifier);
-    // console.log(">> ZIGBEE_RECEIVE_PACKET >", frame);
-
 
   } else if (C.FRAME_TYPE.ZIGBEE_IO_DATA_SAMPLE_RX === frame.type) {
-    //console.log(frame);
 
 
   } else if (C.FRAME_TYPE.REMOTE_COMMAND_RESPONSE === frame.type) {
 
   } else {
-    //console.debug(frame);
     let dataReceived = String.fromCharCode.apply(null, frame.commandData)
-    //console.log(dataReceived);
   }
 
 });
+
 let browserClient;
 io.on('connection', (client) => {
   console.log(client.client.id);
@@ -171,13 +155,6 @@ io.on('connection', (client) => {
 
   client.on('subscribeToPad', (interval) => {
     console.log('client is subscribing to timer with interval ', interval);
-    // setInterval(() => {
-    //   client.emit('pad-event', {
-    //     device: "test device",
-    //     data: Math.round(Math.random()) * 2 - 1
-    //   })
-    //   ;
-    // }, Math.random() * 1000);
   });
 
   client.on("disconnect", () => {
@@ -187,30 +164,27 @@ io.on('connection', (client) => {
 
 const port = 8000;
 io.listen(port);
-console.log('listening on port ', port);
 
-function sleep(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
+function RecoverPassword(){
+  var options = {
+    url: 'https://localhost:8443/access_passes',
+    strictSSL: false,
+    secureProtocol: 'TLSv1_method'
+  }
+
+  request.get(options, function(error, response, body) {
+    if (!error) {
+      let keys = Object.keys(body);
+      let jsonData = JSON.parse(body);
+      let passwordArray = [];
+      for(let i = 0; i< jsonData["hydra:member"].length; i++){
+        passwordArray.push(jsonData["hydra:member"][i]["pass"]);
+      }
+      console.log(passwordArray);
+      return passwordArray;
+    }
+    else {
+      console.log(error);
+    }
+  });
 }
-//
-// serial_xbee.on("data", function(data) {
-//     console.log(data.type);
-//   // console.log('xbee data received:', data.type);
-//   // client.emit('timer', "pouet");
-// //
-// });
-
-// shepherd.on('ready', function () {
-//   console.log('Server is ready.');
-//
-//   // allow devices to join the network within 60 secs
-//   shepherd.permitJoin(60, function (err) {
-//     if (err)
-//       console.log(err);
-//   });
-// });
-//
-// shepherd.start(function (err) {                // start the server
-//   if (err)
-//     console.log(err);
-// });
