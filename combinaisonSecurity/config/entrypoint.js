b80ca20d3364091dbffb10248cfdc58378dce3c3
Ayoub LAAROBI <alaarobi@prismamedia.com>
2020-01-19 10:06:31 +0100

    starting react-native mobile app for configuring security combinaison as 'access pass'

---
+++ combinaisonSecurity/config/entrypoint.js
@@ -0,0 +1,1 @@
+export const ENTRYPOINT = 'https://192.168.1.72:8443';
---
+++ combinaisonSecurity/routes/accesspass.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Scene, Actions } from 'react-native-router-flux';
+import List from '../components/accesspass/List';
+import Create from '../components/accesspass/Create';
+import Show from '../components/accesspass/Show';
+import Update from '../components/accesspass/Update';
+import {delayRefresh} from '../utils/helpers';
+
+export default [
+          <Scene
+              rightTitle="Add"
+              onRight={() => Actions.accesspassCreate()}
+              key="accesspassList" component={List}
+              title="List of AccessPasss"
+              initial
+          />,
+          <Scene key="accesspassCreate" component={Create}
+                 title="Add a new accesspass"/>,
+          <Scene key="accesspassShow" component={Show}
+                 title="AccessPass"
+                 leftTitle="< List of AccessPasss"
+                 onLeft={() => {
+                   Actions.pop();
+                   delayRefresh();
+                 }}/>,
+          <Scene key="accesspassUpdate" component={Update}
+                 title="Update AccessPass"/>,
+];
